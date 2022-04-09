d15134369565d84b31ed787079fbd7425afec24c
Louis Janzen <louis.janzen@alumni.fh-aachen.de>
2022-04-09 15:14:10 +0200

    3.1 mit tests

--- Aufgabe3/Aufgabe3_1.py
+++ Aufgabe3/Aufgabe3_1.py
@@ -14,7 +14,6 @@ def zerlegung(a):
                     found = True
             counter += 1
         elif i < len(a) - 1:
-            print(a)
             p[i] = i
         for k in range(i + 1, len(a)):
             factor = a[k][i] / a[i][i]
@@ -48,14 +47,64 @@ def rueckwaerts(a, x):
         y[len(a) - i - 1] = enumerator / a[len(a) - i - 1][len(a) - i - 1]
     return y
 
+def dimensionsmatrix(n):
+    matrix = np.zeros((n, n), dtype=float)
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            matrix[i][j] = 1 / (i + j + 1)
+    return matrix
+
+
+def dimensionsvector(n):
+    matrix = np.zeros(n, dtype=float)
+    matrix.shape = n
+    for i in range(len(matrix)):
+        matrix[i] = 1 / (i + 2)
+    return matrix
 
 if __name__ == '__main__':
-    arr = np.array([[0, 0, 0, 1], [2, 1, 2, 0], [4, 4, 0, 0], [2, 3, 1, 0]], dtype=float)
-    print(arr)
-    b = np.array([3, 5, 4, 5], dtype=float)
-    lu, p = zerlegung(arr)
-    print(lu)
-    bperm = permutation(p, b)
+    # arr = np.array([[0, 0, 0, 1], [2, 1, 2, 0], [4, 4, 0, 0], [2, 3, 1, 0]], dtype=float)
+    # print("A:")
+    # print(arr)
+    # b = np.array([3, 5, 4, 5], dtype=float)
+    # print("b:")
+    # print(b)
+    # lu, p = zerlegung(arr)
+    # bperm = permutation(p, b)
+    # v = vorwaerts(lu, bperm)
+    # r = rueckwaerts(lu, v)
+    # print("Loesung:")
+    # print(r)
+
+    dimM5 = dimensionsmatrix(5)
+    dimV5 = dimensionsvector(5)
+    dimM10 = dimensionsmatrix(10)
+    dimV10 = dimensionsvector(10)
+    dimM15 = dimensionsmatrix(15)
+    dimV15 = dimensionsvector(15)
+    dimM20 = dimensionsmatrix(20)
+    dimV20 = dimensionsvector(20)
+    lu, p = zerlegung(dimM5)
+    bperm = permutation(p, dimV5)
     v = vorwaerts(lu, bperm)
     r = rueckwaerts(lu, v)
+    print("Loesung mit dim = 5:")
+    print(r)
+    lu, p = zerlegung(dimM10)
+    bperm = permutation(p, dimV10)
+    v = vorwaerts(lu, bperm)
+    r = rueckwaerts(lu, v)
+    print("Loesung mit dim = 10:")
+    print(r)
+    lu, p = zerlegung(dimM15)
+    bperm = permutation(p, dimV15)
+    v = vorwaerts(lu, bperm)
+    r = rueckwaerts(lu, v)
+    print("Loesung mit dim = 15:")
+    print(r)
+    lu, p = zerlegung(dimM20)
+    bperm = permutation(p, dimV20)
+    v = vorwaerts(lu, bperm)
+    r = rueckwaerts(lu, v)
+    print("Loesung mit dim = 20:")
     print(r)
