3119d2257d59364690e5c45819e98a001972a295
Louis Janzen <louis.janzen@alumni.fh-aachen.de>
2022-04-27 22:54:28 +0200

    5.2

---
+++ Aufgabe5/Aufgabe5_2.py
@@ -0,0 +1,21 @@
+from numpy import zeros
+import numpy as np
+
+
+def gs():
+    n = 30
+    iteration = 120
+    f = -1.16
+    d = 1.0
+    e = 0.16
+    x = zeros(n)
+    for i in range(iteration):
+        x[0] = -1*e - f
+        for j in range(1, n - 1):
+            x[j] = (0 - e * x[j - 1] - f * x[j + 1])
+        x[-1] = (-f - e * x[-2])
+    return x
+
+if __name__ == '__main__':
+    gs()
+    print(gs())
