30fd7ec1ee5936014bfbbce3f64e848296b1f55b
Emanuel162 <Spieleaffe162>
2022-05-21 13:18:21 +0200

    continue 8_5

--- Aufgabe8/Aufgabe8_5.py
+++ Aufgabe8/Aufgabe8_5.py
@@ -12,21 +12,33 @@ def dividierteDifferenzen(x_werte, y_werte):
         for j in reversed(range(counter)):
             if i != j:
                 difs[i][j] = (difs[i][j + 1] - difs[i - 1][j]) / (x_werte[i] - x_werte[j])
-                print(f'difs[{i}][{j}] = {difs[i][j]}')
+                # print(f'difs[{i}][{j}] = {difs[i][j]}')
 
         counter = counter + 1
-    print(difs)
     res = np.zeros(len(x_werte))
     for i in range(len(x_werte)):
         res[i] = difs[i][0]
-    print(f'div. Differenzen Diagonale: {res}')
+    # print(f'div. Differenzen Diagonale: {res}')
+    return res
+
+
+def hornerSchema(x_werte, dividierte, x):
+    res = 0
+    for i in reversed(range(1, len(x_werte))):
+        res += dividierte[i]
+
+        for j in x_werte-1:
+            res *= (x-x_werte[j])
+        print(res)
     return res
 
 
 def main():
     x_werte = np.array([0, 1, 3])
     y_werte = np.array([3, 2, 6])
-    dividierteDifferenzen(x_werte, y_werte)
+    dividierte = dividierteDifferenzen(x_werte, y_werte)
+    x = 2
+    print(f'Das Interpolationspolynom an der Stelle x = {x} ist {hornerSchema(x_werte, dividierte, x)}.')
 
 
 if __name__ == '__main__':
